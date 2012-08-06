43f9e3ff86f629bfc351afbcee73660537d9fb18
guenter.platzerworld@googlemail.com <guenter.platzerworld@googlemail.com@d2d14e04-5fcc-56b1-cc32-140369a3c793>
2012-08-06 14:48:26 +0000

    BiergartenVO mit Gson

--- trunk/BiergartenService/src/com/platzerworld/biergarten/BiergartenServiceServlet.java
+++ trunk/BiergartenService/src/com/platzerworld/biergarten/BiergartenServiceServlet.java
@@ -2,41 +2,31 @@ package com.platzerworld.biergarten;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.platzerworld.biergarten.dao.EMFService;
 import com.platzerworld.biergarten.model.Biergarten;
 
 
 @SuppressWarnings("serial")
 public class BiergartenServiceServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		List<Biergarten> biergartenTOs = new ArrayList<Biergarten>();
-		
-		resp.setContentType("application/json");
+		// resp.setContentType("application/json");
 		PrintWriter out = resp.getWriter();
 		
-		Biergarten bg = null;
-		bg = new Biergarten();bg.name="Zum Aumeister";
-		bg.strasse="Sondermeierstrasse 1";
-		bg.plz="80939";
-		bg.ort="München (Schwabing)";
-		bg.telefon="089 / 18 93 142 0";
-		bg.email="aumeister@aumeister.de";
-		bg.url="www.aumeister.de";
-		bg.latitude="48.185977";
-		bg.longitude="11.620038";
-		bg.desc="Am Nordrand des Englischen Gartens gelegen";
-		bg.favorit=false;
-		biergartenTOs.add(bg);
+		//EntityManager em = EMFService.get().createEntityManager();
+		//Query q = em.createQuery("select t from Todo t where t.author = :userId");
+		//q.setParameter("userId", userId);
 		
-		com.google.appengine.repackaged.com.google.gson.Gson gson = new com.google.appengine.repackaged.com.google.gson.Gson();
-		String data = gson.toJson(biergartenTOs);
-		out.println(data);	
+		//javax.persistence.Query q = em.createQuery("select b from Biergarten b");
+		//List<Biergarten> todos = q.getResultList();
+		
+		out.println("Finish!");
 	}
 	
 	private String checkNull(String s) {
--- trunk/BiergartenService/src/com/platzerworld/biergarten/services/BiergartenService.java
+++ trunk/BiergartenService/src/com/platzerworld/biergarten/services/BiergartenService.java
@@ -1,14 +1,10 @@
 package com.platzerworld.biergarten.services;
 
 import java.lang.reflect.Type;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -28,7 +24,8 @@ public class BiergartenService {
 	public String insertBiergarten() {
 		EntityManager em = EMFService.get().createEntityManager();
 		Biergarten bg = null;
-		bg = new Biergarten();bg.name="Zum Aumeister";
+		bg = new Biergarten();
+		bg.name="Zum Aumeister";
 		bg.strasse="Sondermeierstrasse 1";
 		bg.plz="80939";
 		bg.ort="M�nchen (Schwabing)";
@@ -50,27 +47,42 @@ public class BiergartenService {
 	@Produces("text/plain")
 	@Path("/holebiergarten")
 	public String getBiergarten() {
-		//return "holebiergarten";
-		
-		
 		List<BiergartenVO> biergartenTOs = new ArrayList<BiergartenVO>();
 		BiergartenVO bg = null;
-		bg = new BiergartenVO();
-		bg.name="Zum Aumeister";
-		bg.strasse="Sondermeierstrasse 1";
-		bg.plz="80939";
-		bg.ort="München (Schwabing)";
-		bg.telefon="089 / 18 93 142 0";
-		bg.email="aumeister@aumeister.de";
-		bg.url="www.aumeister.de";
-		bg.latitude="48.185977";
-		bg.longitude="11.620038";
-		bg.desc="Am Nordrand des Englischen Gartens gelegen";
-		bg.favorit=false;
-		biergartenTOs.add(bg);
 		
+		EntityManager em = EMFService.get().createEntityManager();
+		Query q = em.createQuery("select b from Biergarten b");
+		
+				
+		List<Biergarten> biergaerten = q.getResultList();
+		for (Biergarten biergarten : biergaerten) {
+			bg = new BiergartenVO();
+			bg.id = biergarten.id;
+			bg.name = biergarten.name;
+			bg.strasse = biergarten.strasse;
+			bg.plz = biergarten.plz;
+			bg.ort = biergarten.ort;
+			bg.telefon = biergarten.telefon;
+			bg.email = biergarten.email;
+			bg.url = biergarten.url;
+			bg.latitude = biergarten.latitude;
+			bg.longitude = biergarten.longitude;
+			bg.desc = biergarten.desc;
+			bg.desclong = biergarten.descLong;
+			bg.mass = biergarten.mass;
+			bg.apfelschorle = biergarten.apfelschorle;
+			bg.riesenbreze = biergarten.riesenbreze;
+			bg.obazda = biergarten.obazda;
+			bg.biermarke = biergarten.biermarke;
+			bg.lieblingsgericht = biergarten.lieblingsgericht;
+			bg.speisenkommentar = biergarten.speisekommentar;
+			bg.favorit = biergarten.favorit;
+			biergartenTOs.add(bg);
+		}
+		 
 		com.google.appengine.repackaged.com.google.gson.Gson gson = new com.google.appengine.repackaged.com.google.gson.Gson();
 		String data = gson.toJson(biergartenTOs);
+		
 		
 		return data;
 		
