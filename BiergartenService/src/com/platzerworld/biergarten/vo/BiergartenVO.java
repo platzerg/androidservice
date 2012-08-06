3ee3ef00f05abb3dfe052f4aba2e48073273c75e
guenter.platzerworld@googlemail.com <guenter.platzerworld@googlemail.com@d2d14e04-5fcc-56b1-cc32-140369a3c793>
2012-08-06 08:54:12 +0000

    Biergartenservice

---
+++ BiergartenService/src/com/platzerworld/biergarten/vo/BiergartenVO.java
@@ -0,0 +1,59 @@
+package com.platzerworld.biergarten.vo;
+
+
+
+/**
+ * Value-Object SpielerVO
+ * 
+ * @author platzerg
+ */
+public class BiergartenVO{
+	private static final long serialVersionUID = -8854310183206811021L;
+
+	public long id;
+
+	public String name;
+	
+	public String strasse;
+	
+	public String plz;
+	
+	public String ort;
+	
+	public String telefon;
+	
+	public String email;
+	
+	public String url;
+	
+	public String latitude;
+	
+	public String longitude;
+	
+	public String desc;
+	
+	public String desclong;
+	
+	public String mass;
+	
+	public String apfelschorle;
+	
+	public String riesenbreze;
+	
+	public String obazda;
+	
+	public String biermarke;
+	
+	public String lieblingsgericht;
+	
+	public String speisenkommentar;
+	
+	public boolean favorit;
+	
+	
+	
+
+	public BiergartenVO(){
+		
+	}
+}
